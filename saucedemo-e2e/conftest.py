80c01bf38f8f433249b13722d50426cf936ad8f3
Aline <alinediasmarquesramos638@gmail.com>
2026-05-30 16:01:51 -0300

    ULTIMO TESTE COM FE EM DEUS

--- saucedemo-e2e/conftest.py
+++ saucedemo-e2e/conftest.py
@@ -8,15 +8,22 @@ from webdriver_manager.chrome import ChromeDriverManager
 def driver():
     
     options = Options()
-    options.add_argument('--headless')           
-    options.add_argument('--no-sandbox')         
-    options.add_argument('--disable-dev-shm-usage')  
-    options.add_argument('--window-size=1920,1080') 
-    options.add_argument('--disable-features=PasswordManager')
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--disable-features=PasswordManager,AutofillServerCommunication')
+    options.add_argument('--disable-save-password-bubble')
+    options.add_argument('--disable-popup-blocking')
+    options.add_argument('--disable-notifications')
     options.add_experimental_option('prefs', {
         'credentials_enable_service': False,
-        'profile.password_manager_enabled': False
-})
+        'profile.password_manager_enabled': False,
+        'profile.default_content_setting_values.notifications': 2,
+        'autofill.profile_enabled': False,
+    })
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+
 
     
     driver = webdriver.Chrome(
