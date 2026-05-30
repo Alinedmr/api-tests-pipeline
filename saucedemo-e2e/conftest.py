44c48f2637827f715a287fe833803ebdef220a7e
Aline <alinediasmarquesramos638@gmail.com>
2026-05-30 16:26:31 -0300

    fui burr comentei o headless

--- saucedemo-e2e/conftest.py
+++ saucedemo-e2e/conftest.py
@@ -8,9 +8,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 def driver():
     
     options = Options()
-    # options.add_argument('--headless')
-    # options.add_argument('--no-sandbox')
-    # options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--window-size=1920,1080')
     options.add_argument('--disable-features=PasswordManager,AutofillServerCommunication')
     options.add_argument('--disable-save-password-bubble')
